91dd32a804ffc4542347cadf897f305f8a478485
MBoutray <martindeboutray@hotmail.fr>
2023-10-27 16:47:07 +0200

    setup library to use in npm

---
+++ src/components/InputText.vue
@@ -0,0 +1,13 @@
+<template>
+  <input type="text" :value="value" :placeholder="placeholder" :disabled="disabled" />
+</template>
+
+<script>
+export default {
+  props: {
+    value: null,
+    placeholder: null,
+    disabled: false
+  }
+}
+</script>
